83c6b4879c222400374b77d77bb18ed0f8968274
Evert Moreno Serrate <evert23122004@gmail.com>
2025-09-05 23:09:52 -0400

    Task 3: Hello world!
    - Hacer el saludo, nombre completo + materia
    - Mostrar peso, altura, edad, variable booleana si edad es un número par
    - poner género con algo que NO sea un char ni ninguna estructura de dato que hayamos visto hasta ahora

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,25 @@
 fun main() {
-    val hello = "Hello Kotlin"
-    println(hello)
+    val name = "Evert Moreno Serrate"
+    val subject = "Aplicaciones móviles"
+    val weight = 71.5
+    val height = 1.8
+    val age = 20
+    val isAgeOdd : Boolean = age % 2 == 0
+    val myGender = Gender.Male
+
+    val message = """
+        ¡Hola! Soy $name
+        Materia: $subject
+        Peso: $weight
+        Altura: $height
+        Edad: $age
+        Edad es par: $isAgeOdd
+        Género: $myGender
+    """.trimIndent()
+    println(message)
+}
+
+enum class Gender {
+    Male,
+    Female
 }
