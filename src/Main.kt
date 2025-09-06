ea0ae7bbfdad1839b9ed4ff05909dc20edc4e93d
Evert Moreno Serrate <evert23122004@gmail.com>
2025-09-05 23:48:14 -0400

    Task 3: Funciones y procedimientos
    - Función (procedimiento) "saludar(saludo, nombreCompleto)" que reciba como parámetros el saludo y el nombre
    - Saludo por defecto debe ser hola, si no está vacío mostrar el que se pasa por parámetro
    - Función "mostrarGenero(genero: Genero): String"

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
+enum class Gender {
+    Masculino,
+    Femenino
+}
+
+fun saludar(saludo : String = "Hola", nombreCompleto: String) {
+    println("$saludo, soy $nombreCompleto")
+}
+
+fun mostrarGenero(gender: Gender) : String {
+    return "Género: $gender"
+}
+
 fun main() {
     val name = "Evert Moreno Serrate"
     val subject = "Aplicaciones móviles"
@@ -5,9 +18,9 @@ fun main() {
     val height = 1.8
     val age = 20
     val isAgeOdd : Boolean = age % 2 == 0
-    val myGender = Gender.Male
+    val myGender = Gender.Masculino
 
-    val message = """
+    /*val message = """
         ¡Hola! Soy $name
         Materia: $subject
         Peso: $weight
@@ -15,11 +28,9 @@ fun main() {
         Edad: $age
         Edad es par: $isAgeOdd
         Género: $myGender
-    """.trimIndent()
-    println(message)
-}
-
-enum class Gender {
-    Male,
-    Female
+    """.trimIndent()*/
+    saludar(nombreCompleto = name)
+    saludar("Buenas tardes", name)
+    println(mostrarGenero(myGender))
+    //println(message)
 }
