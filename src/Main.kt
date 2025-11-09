d3873ae8d57e10b420e539706ef21c860ad3a04b
Evert Moreno Serrate <evert23122004@gmail.com>
2025-11-08 22:30:42 -0400

    Feat: implementación de patrón de diseño Singleton
    Implementación de patrón de diseño Singleton en Kotlin con inicialización perezosa, con la palabra clave object

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,44 @@
+object Logger {
+
+    // Una propiedad privada para guardar los logs
+    private val logHistory = mutableListOf<String>()
+
+    init {
+        println("[Logger]: Sistema de Log inicializado.")
+        logHistory.add("--- Inicio del Log ---")
+    }
+
+    fun log(message: String) {
+        val entry = "[INFO]: $message"
+        println(entry)
+        logHistory.add(entry)
+    }
+    
+    fun printHistory() {
+        println("\n--- Historial de Logs ---")
+        logHistory.forEach { println(it) }
+    }
+}
+
 fun main() {
-    println("Hello World!")
+    
+    println("--- Simulación de App iniciada ---")
+
+    println("\n[Pantalla Perfil]: Abriendo pantalla...")
+    Logger.log("Usuario 'eve' ha entrado al perfil.")
+    Logger.log("Cargando foto de perfil.")
+    
+    
+    println("\n[Pantalla Tienda]: Abriendo tienda...")
+    Logger.log("Cargando lista de productos.")
+    Logger.log("Producto 'Kotlin Book' añadido al carrito.")
+
+    
+    println("\n--- Simulación de App terminada ---")
+    Logger.printHistory()
+    
+    // Demostración de la instancia única
+    val ref1 = Logger
+    val ref2 = Logger
+    println("\n¿Logger 1 y Logger 2 son la misma instancia? ${ref1 === ref2}") // Imprimirá: true
 }
